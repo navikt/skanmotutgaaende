7ee4689e65053e2e01e4fd36eb3fa1688f0bff28
Marton Skjæveland <Marton.Skjeveland@nav.no>
2020-04-14 13:26:09 +0200

    Fikset if test i export-vault-secrets

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-if test -f /var/run/secrets/nais.io/skanmotutgaaende/username;
+if test -f /var/run/secrets/nais.io/srvskanmotutgaaende/username;
 then
     echo "Setting SERVICEUSER_USERNAME"
     export SKANMOTUTGAAENDE_SERVICEUSER_USERNAME=$(cat /var/run/secrets/nais.io/skanmotutgaaende/username)
 fi
 
-if test -f /var/run/secrets/nais.io/skanmotutgaaende/password;
+if test -f /var/run/secrets/nais.io/srvskanmotutgaaende/password;
 then
     echo "Setting SERVICEUSER_PASSWORD"
     export SKANMOTUTGAAENDE_SERVICEUSER_***passord=gammelt_passord***)
