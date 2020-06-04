af3505633a9728760189db0ffe6bffa71d0fd529
Marton Skjæveland <Marton.Skjeveland@nav.no>
2020-06-04 15:25:32 +0200

    MMA-4519: Ryddet opp og svar paa PR

--- core/src/main/java/no/nav/skanmotutgaaende/domain/SkanningAvUtgaaende.java
+++ core/src/main/java/no/nav/skanmotutgaaende/domain/SkanningAvUtgaaende.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SkanningAvUtgaende {
+public class SkanningAvUtgaaende {
     private Skanningmetadata skanningmetadata;
 }
