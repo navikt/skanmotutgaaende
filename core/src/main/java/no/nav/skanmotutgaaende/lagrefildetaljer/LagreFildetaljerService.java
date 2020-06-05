0f4176ae787036030e416d9f2069ad6c5bd6e8ba
Marton Skjæveland <Marton.Skjeveland@nav.no>
2020-06-05 12:26:54 +0200

    MMA-4519: Flyttet feilhåndtering av opplasting opp et nivaa

--- core/src/main/java/no/nav/skanmotutgaaende/lagrefildetaljer/LagreFildetaljerService.java
+++ core/src/main/java/no/nav/skanmotutgaaende/lagrefildetaljer/LagreFildetaljerService.java
@@ -2,15 +2,11 @@ package no.nav.skanmotutgaaende.lagrefildetaljer;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.skanmotutgaaende.domain.Filepair;
-import no.nav.skanmotutgaaende.domain.Journalpost;
 import no.nav.skanmotutgaaende.domain.Skanningmetadata;
-import no.nav.skanmotutgaaende.exceptions.functional.AbstractSkanmotutgaaendeFunctionalException;
-import no.nav.skanmotutgaaende.exceptions.technical.AbstractSkanmotutgaaendeTechnicalException;
 import no.nav.skanmotutgaaende.lagrefildetaljer.data.LagreFildetaljerRequest;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
-import java.util.Optional;
 
 @Service
 @Slf4j
@@ -25,28 +21,12 @@ public class LagreFildetaljerService {
         this.lagreFildetaljerRequestMapper = new LagreFildetaljerRequestMapper();
     }
 
-    public boolean lagreFildetaljer(Optional<Skanningmetadata> skanningmetadata, Filepair filepair) {
-        if (skanningmetadata.isEmpty()) {
-            return false;
-        }
-        String batchnavn = skanningmetadata.map(Skanningmetadata::getJournalpost).map(Journalpost::getBatchnavn).orElse(null);
-        try {
-            String jpid = skanningmetadata.get().getJournalpost().getJournalpostId();
-            log.info("Skanmotutgaaende lagrer fildetaljer for journalpost, id={}, fil={}, batch={}", jpid, filepair.getName(), batchnavn);
-            LagreFildetaljerRequest request = lagreFildetaljerRequestMapper.mapMetadataToLagreFildetaljerRequest(skanningmetadata.get(), filepair);
-            lagreFildetaljerConsumer.lagreFilDetaljer(request, jpid);
-            return true;
+    public void lagreFildetaljer(Skanningmetadata skanningmetadata, Filepair filepair) {
+        String jpid = skanningmetadata.getJournalpost().getJournalpostId();
+        log.info("Skanmotutgaaende lagrer fildetaljer for journalpost, id={}, fil={}, batch={}", jpid, filepair.getName(), skanningmetadata.getJournalpost().getBatchnavn());
+        LagreFildetaljerRequest request = lagreFildetaljerRequestMapper.mapMetadataToLagreFildetaljerRequest(skanningmetadata, filepair);
+        lagreFildetaljerConsumer.lagreFilDetaljer(request, jpid);
 
-        } catch (AbstractSkanmotutgaaendeFunctionalException e) {
-            log.warn("Skanmotutgaaende feilet funksjonelt med lagring av fildetaljer fil={}, batch={}", filepair.getName(), batchnavn, e);
-            return false;
-        } catch (AbstractSkanmotutgaaendeTechnicalException e) {
-            log.warn("Skanmotutgaaende feilet teknisk med lagring av fildetaljer fil={}, batch={}", filepair.getName(), batchnavn, e);
-            return false;
-        } catch (Exception e) {
-            log.warn("Skanmotutgaaende feilet med ukjent feil ved lagring av fildetaljer fil={}, batch={}", filepair.getName(), batchnavn, e);
-            return false;
-        }
     }
 
 }
