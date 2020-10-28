54e741405338270a3254c0bff44f50d56041f4d5
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-10-28 14:19:30 +0100

    MMA-4694 Opppdatert dekryptering streams

---
+++ core/src/main/java/no/nav/skanmotutgaaende/CustomZipIterator.java
@@ -0,0 +1,140 @@
+package no.nav.skanmotutgaaende;
+
+import net.lingala.zip4j.io.inputstream.ZipInputStream;
+import net.lingala.zip4j.model.LocalFileHeader;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.RuntimeCamelException;
+import org.apache.camel.support.DefaultMessage;
+import org.apache.camel.util.IOHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedInputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.zip.ZipEntry;
+
+public class CustomZipIterator implements Iterator<Message>, Closeable {
+    static final Logger LOGGER = LoggerFactory.getLogger(org.apache.camel.dataformat.zipfile.ZipIterator.class);
+
+    private final Exchange exchange;
+    private boolean allowEmptyDirectory;
+    private volatile ZipInputStream zipInputStream;
+    private volatile Message parent;
+
+    public CustomZipIterator(Exchange exchange, InputStream inputStream) {
+        this.exchange = exchange;
+        this.allowEmptyDirectory = false;
+        if (inputStream instanceof ZipInputStream) {
+            zipInputStream = (ZipInputStream) inputStream;
+        } else {
+            zipInputStream = new ZipInputStream(new BufferedInputStream(inputStream));
+        }
+        parent = null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        try {
+            if (zipInputStream == null) {
+                return false;
+            }
+            boolean availableDataInCurrentEntry = zipInputStream.available() == 1;
+            if (!availableDataInCurrentEntry) {
+                // advance to the next entry.
+                parent = getNextElement();
+                if (parent == null) {
+                    zipInputStream.close();
+                    availableDataInCurrentEntry = false;
+                } else {
+                    availableDataInCurrentEntry = true;
+                }
+            }
+            return availableDataInCurrentEntry;
+        } catch (IOException exception) {
+            throw new RuntimeCamelException(exception);
+        }
+    }
+
+    @Override
+    public Message next() {
+        if (parent == null) {
+            parent = getNextElement();
+        }
+        Message answer = parent;
+        parent = null;
+        checkNullAnswer(answer);
+
+        return answer;
+    }
+
+    private Message getNextElement() {
+        if (zipInputStream == null) {
+            return null;
+        }
+
+        try {
+            LocalFileHeader current = getNextEntry();
+
+            if (current != null) {
+                LOGGER.debug("read zipEntry {}", current.getFileName());
+                Message answer = new DefaultMessage(exchange.getContext());
+                answer.getHeaders().putAll(exchange.getIn().getHeaders());
+                answer.setHeader("zipFileName", current.getFileName());
+                answer.setHeader(Exchange.FILE_NAME, current.getFileName());
+                answer.setBody(new ZipInputStreamWrapper(zipInputStream));
+                return answer;
+            } else {
+                LOGGER.trace("close zipInputStream");
+                return null;
+            }
+        } catch (IOException exception) {
+            throw new RuntimeCamelException(exception);
+        }
+    }
+
+    public void checkNullAnswer(Message answer) {
+        if (answer == null && zipInputStream != null) {
+            IOHelper.close(zipInputStream);
+            zipInputStream = null;
+        }
+    }
+
+    private LocalFileHeader getNextEntry() throws IOException {
+        LocalFileHeader entry;
+
+        while ((entry = zipInputStream.getNextEntry()) != null) {
+            if (!entry.isDirectory()) {
+                return entry;
+            } else {
+                if (allowEmptyDirectory) {
+                    return entry;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void close() throws IOException {
+        IOHelper.close(zipInputStream);
+        zipInputStream = null;
+    }
+
+    public boolean isSupportIteratorForEmptyDirectory() {
+        return allowEmptyDirectory;
+    }
+
+    public void setAllowEmptyDirectory(boolean allowEmptyDirectory) {
+        this.allowEmptyDirectory = allowEmptyDirectory;
+    }
+}
--- core/src/main/java/no/nav/skanmotutgaaende/CustomZipSplitter.java
+++ core/src/main/java/no/nav/skanmotutgaaende/CustomZipSplitter.java
@@ -1,5 +1,6 @@
 package no.nav.skanmotutgaaende;
 
+import lombok.extern.slf4j.Slf4j;
 import net.lingala.zip4j.io.inputstream.ZipInputStream;
 import org.apache.camel.Exchange;
 import org.apache.camel.Expression;
@@ -16,43 +17,44 @@ import java.io.InputStream;
 import java.security.Key;
 import java.util.Base64;
 
+@Slf4j
 public class CustomZipSplitter extends ZipSplitter {
-	public CustomZipSplitter() {
-	}
+    public CustomZipSplitter() {
+    }
 
-	@Override
-	public Object evaluate(Exchange exchange) {
-		try{
-			Message inputMessage = exchange.getIn();
-			String zipPassword = "password";
+    @Override
+    public Object evaluate(Exchange exchange) {
+        try {
+            Message inputMessage = exchange.getIn();
+            String zipPassword = "password";
+            ZipInputStream zip = new ZipInputStream(inputMessage.getBody(InputStream.class), zipPassword.toCharArray());
 
-			ZipInputStream zip = new ZipInputStream(exchange.getIn().getBody(InputStream.class), zipPassword.toCharArray());
+            return new CustomZipIterator(exchange, zip);
+        } catch (Exception e) {
+            //TODO: FIX
+            log.error("Feilet under dekryptering", e);
+            return null;
+        }
+    }
 
-			return new ZipIterator(exchange,zip);
-		}catch (Exception e){
-			//TODO: FIX
-			return null;
-		}
-	}
-
-	/*@Override
-	public Object evaluate(Exchange exchange) {
-		try{
-			Message inputMessage = exchange.getIn();
-			byte[] decodedKey = Base64.getDecoder().decode("123");
-			SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
-			//3505bb564abaa37555ed24bf4d8f70f0
-			SecretKeySpec secKey = new SecretKeySpec("3505bb564abaa37555ed24bf4d8f70f0".getBytes(), "AES");
-			final Cipher c2 = Cipher.getInstance("AES");
-			c2.init(Cipher.DECRYPT_MODE, secKey);
-			return new ZipIterator(exchange, new CipherInputStream(inputMessage.getBody(InputStream.class), c2));
-		}catch (Exception e){
-			return null;
-		}
-	}*/
-	@Override
-	public <T> T evaluate(Exchange exchange, Class<T> type) {
-		Object result = this.evaluate(exchange);
-		return exchange.getContext().getTypeConverter().convertTo(type, exchange, result);
-	}
+    /*@Override
+    public Object evaluate(Exchange exchange) {
+        try{
+            Message inputMessage = exchange.getIn();
+            byte[] decodedKey = Base64.getDecoder().decode("123");
+            SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+            //3505bb564abaa37555ed24bf4d8f70f0
+            SecretKeySpec secKey = new SecretKeySpec("3505bb564abaa37555ed24bf4d8f70f0".getBytes(), "AES");
+            final Cipher c2 = Cipher.getInstance("AES");
+            c2.init(Cipher.DECRYPT_MODE, secKey);
+            return new ZipIterator(exchange, new CipherInputStream(inputMessage.getBody(InputStream.class), c2));
+        }catch (Exception e){
+            return null;
+        }
+    }*/
+    @Override
+    public <T> T evaluate(Exchange exchange, Class<T> type) {
+        Object result = this.evaluate(exchange);
+        return exchange.getContext().getTypeConverter().convertTo(type, exchange, result);
+    }
 }
