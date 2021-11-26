c98ea61a557f3a554cfd5b520cfe2a7869091c9e
A157857 <tim.aasen@nav.no>
2021-11-26 23:31:02 +0100

    mma-5316: Endringer til itest

---
+++ core/src/main/java/no/nav/skanmotutgaaende/lagrefildetaljer/RetryConstants.java
@@ -0,0 +1,6 @@
+package no.nav.skanmotutgaaende.lagrefildetaljer;
+
+public final class RetryConstants {
+	public static final int RETRY_RETRIES = 5;
+	public static final int RETRY_DELAY = 500;
+}
--- core/src/test/java/no/nav/skanmotutgaaende/itest/PostboksUtgaaendeRouteEncryptedIT.java
+++ core/src/test/java/no/nav/skanmotutgaaende/itest/PostboksUtgaaendeRouteEncryptedIT.java
@@ -41,216 +41,198 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = TestConfig.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.cloud.vault.token=123456")
-@AutoConfigureWireMock(port = 0)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ActiveProfiles("itest")
-public class PostboksUtgaaendeRouteEncryptedIT {
+public class PostboksUtgaaendeRouteEncryptedIT extends AbstractItest{
 
-    public static final String INNGAAENDE = "inngaaende";
-    public static final String FEILMAPPE = "feilmappe";
+	public static final String INNGAAENDE = "inngaaende";
+	public static final String FEILMAPPE = "feilmappe";
 
-    private final String URL_DOKARKIV_JOURNALPOST_GEN = "/rest/intern/journalpostapi/v1/journalpost/\\d+/mottaDokumentUtgaaendeSkanning";
-    private final String URL_DOKARKIV_JOURNALPOST_BAD_REQUEST = "/rest/intern/journalpostapi/v1/journalpost/4000004/mottaDokumentUtgaaendeSkanning";
-    private final String ZIP_FILE_NAME_NO_EXTENSION = "01.07.2020_R123456789_1_1000";
-    private final String ZIP_FILENAME_NO_EXTENSION_BAD_PASSWORD = "29.10.2020_R123456789_6_9999";
-    private final String ZIP_FILENAME_NO_EXTENSION_BAD_ENCRYPTION = "01.07.2020_R123456789_2_1000";
-    private final String ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION = "01.07.2020_R100000000_1_1000_ordered_xml_first_big";
-    private final String ZIP_FILE_NAME_NOT_ENCRYPTED_ENC = "01.07.2020_R123456789_1_1000_NotEncrypted";
+		private final String ZIP_FILE_NAME_NO_EXTENSION = "01.07.2020_R123456789_1_1000";
+	private final String ZIP_FILENAME_NO_EXTENSION_BAD_PASSWORD = "29.10.2020_R123456789_6_9999";
+	private final String ZIP_FILENAME_NO_EXTENSION_BAD_ENCRYPTION = "01.07.2020_R123456789_2_1000";
+	private final String ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION = "01.07.2020_R100000000_1_1000_ordered_xml_first_big";
+	private final String ZIP_FILE_NAME_NOT_ENCRYPTED_ENC = "01.07.2020_R123456789_1_1000_NotEncrypted";
 
-    @Inject
-    private Path sshdPath;
+	@Inject
+	private Path sshdPath;
 
-    @BeforeEach
-    void beforeEach() throws IOException {
-        final Path inngaaende = sshdPath.resolve(INNGAAENDE);
-        final Path processed = inngaaende.resolve("processed");
-        final Path feilmappe = sshdPath.resolve(FEILMAPPE);
-        try {
-            preparePath(inngaaende);
-        } catch (Exception e) {
-            //noop. Windows sliter med å slette filene, de blir kun satt til "unavailable"
-        } try {
-            preparePath(processed);
-        } catch (Exception e) {
-            //noop. Windows sliter med å slette filene, de blir kun satt til "unavailable"
-        }
-        try {
-            preparePath(feilmappe);
-        } catch (Exception e) {
-            //noop. Windows sliter med å slette filene, de blir kun satt til "unavailable"
-        }
-    }
+	@BeforeEach
+	void beforeEach() {
+		final Path inngaaende = sshdPath.resolve(INNGAAENDE);
+		final Path processed = inngaaende.resolve("processed");
+		final Path feilmappe = sshdPath.resolve(FEILMAPPE);
+		try {
+			preparePath(inngaaende);
+		} catch (Exception e) {
+			//noop. Windows sliter med å slette filene, de blir kun satt til "unavailable"
+		}
+		try {
+			preparePath(processed);
+		} catch (Exception e) {
+			//noop. Windows sliter med å slette filene, de blir kun satt til "unavailable"
+		}
+		try {
+			preparePath(feilmappe);
+		} catch (Exception e) {
+			//noop. Windows sliter med å slette filene, de blir kun satt til "unavailable"
+		}
+	}
 
-    @AfterEach
-    void tearDown() {
-        WireMock.reset();
-        WireMock.resetAllRequests();
-        WireMock.removeAllMappings();
-        File dir = sshdPath.toFile();
-        for (File file : dir.listFiles()) {
-            file.delete();
-        }
-        File dirr = new File(String.valueOf(sshdPath.toAbsolutePath()));
-        dirr.delete();
-    }
+	@AfterEach
+	void tearDown() {
+		WireMock.reset();
+		WireMock.resetAllRequests();
+		WireMock.removeAllMappings();
+		File dir = sshdPath.toFile();
+		for (File file : dir.listFiles()) {
+			file.delete();
+		}
+		File dirr = new File(String.valueOf(sshdPath.toAbsolutePath()));
+		dirr.delete();
+	}
 
-    @Test
-    public void shouldLesOgLagreHappy() throws IOException {
-        // 01.07.2020_R123456789_1_1000.zip
-        // OK   - 01.07.2020_R123456789_0001
-        // OK   - 01.07.2020_R123456789_0002 (mangler filnavn og fysiskPostboks)
-        // FEIL - 01.07.2020_R123456789_0003 (valideringsfeil, mangler journalpostid)
-        // FEIL - 01.07.2020_R123456789_0004 (vil feile hos dokarkiv 400_Bad_Request)
-        // FEIL - 01.07.2020_R123456789_0005 (mangler pdf)
-        // FEIL - 01.07.2020_R123456789_0006 (mangler xml)
-        setUpHappyStubs();
-        setUpBadStubs();
-        copyFileFromClasspathToInngaaende(ZIP_FILE_NAME_NO_EXTENSION + ".enc.zip");
+	@Test
+	public void shouldLesOgLagreHappy() throws IOException {
+		// 01.07.2020_R123456789_1_1000.zip
+		// OK   - 01.07.2020_R123456789_0001
+		// OK   - 01.07.2020_R123456789_0002 (mangler filnavn og fysiskPostboks)
+		// FEIL - 01.07.2020_R123456789_0003 (valideringsfeil, mangler journalpostid)
+		// FEIL - 01.07.2020_R123456789_0004 (vil feile hos dokarkiv 400_Bad_Request)
+		// FEIL - 01.07.2020_R123456789_0005 (mangler pdf)
+		// FEIL - 01.07.2020_R123456789_0006 (mangler xml)
+		setUpHappyStubs();
+		setUpBadStubs();
+		copyFileFromClasspathToInngaaende(ZIP_FILE_NAME_NO_EXTENSION + ".enc.zip");
 
-        await().atMost(15, SECONDS).untilAsserted(() -> {
-            try {
-                assertThat(Files.list(sshdPath.resolve(FEILMAPPE)
-                        .resolve(ZIP_FILE_NAME_NO_EXTENSION))
-                        .collect(Collectors.toList())).hasSize(4);
-            } catch (NoSuchFileException e) {
-                fail();
-            }
-        });
+		await().atMost(15, SECONDS).untilAsserted(() -> {
+			try {
+				assertThat(Files.list(sshdPath.resolve(FEILMAPPE)
+						.resolve(ZIP_FILE_NAME_NO_EXTENSION))
+						.collect(Collectors.toList())).hasSize(4);
+			} catch (NoSuchFileException e) {
+				fail();
+			}
+		});
 
-        final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE).resolve(ZIP_FILE_NAME_NO_EXTENSION))
-                .map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
-                .collect(Collectors.toList());
-        assertTrue(feilmappeContents.containsAll(List.of(
-                "01.07.2020_R123456789_0003.enc.zip",
-                "01.07.2020_R123456789_0004.enc.zip",
-                "01.07.2020_R123456789_0005.enc.zip",
-                "01.07.2020_R123456789_0006.enc.zip"
-        )));
-        verify(exactly(3), putRequestedFor(urlMatching(URL_DOKARKIV_JOURNALPOST_GEN)));
-    }
+		final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE).resolve(ZIP_FILE_NAME_NO_EXTENSION))
+				.map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
+				.collect(Collectors.toList());
+		assertTrue(feilmappeContents.containsAll(List.of(
+				"01.07.2020_R123456789_0003.enc.zip",
+				"01.07.2020_R123456789_0004.enc.zip",
+				"01.07.2020_R123456789_0005.enc.zip",
+				"01.07.2020_R123456789_0006.enc.zip"
+		)));
+		verify(exactly(3), putRequestedFor(urlMatching(URL_DOKARKIV_JOURNALPOST_GEN)));
+	}
 
-    @Test
-    public void shouldBehandleZipXmlOrderedLastWithinCompletionTimeout() throws IOException {
-        // 01.07.2020_R100000000_1_1000_ordered_xml_first_big.zip
-        // OK   - 01.07.2020_R100000000_0001
-        // OK   - 01.07.2020_R100000000_0002 (mangler filnavn og fysiskPostboks)
-        // FEIL - 01.07.2020_R100000000_0003 (valideringsfeil, mangler journalpostid)
-        // FEIL - 01.07.2020_R100000000_0004 (vil feile hos dokarkiv 400_Bad_Request)
-        // FEIL - 01.07.2020_R100000000_0005 (mangler pdf)
-        // FEIL - 01.07.2020_R100000000_0006 (mangler xml)
-        // OK   - 01.07.2020_R100000000_0007
-        // ...
-        // OK   - 01.07.2020_R100000000_0059
-        setUpHappyStubs();
-        setUpBadStubs();
-        copyFileFromClasspathToInngaaende(ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION + ".enc.zip");
+	@Test
+	public void shouldBehandleZipXmlOrderedLastWithinCompletionTimeout() throws IOException {
+		// 01.07.2020_R100000000_1_1000_ordered_xml_first_big.zip
+		// OK   - 01.07.2020_R100000000_0001
+		// OK   - 01.07.2020_R100000000_0002 (mangler filnavn og fysiskPostboks)
+		// FEIL - 01.07.2020_R100000000_0003 (valideringsfeil, mangler journalpostid)
+		// FEIL - 01.07.2020_R100000000_0004 (vil feile hos dokarkiv 400_Bad_Request)
+		// FEIL - 01.07.2020_R100000000_0005 (mangler pdf)
+		// FEIL - 01.07.2020_R100000000_0006 (mangler xml)
+		// OK   - 01.07.2020_R100000000_0007
+		// ...
+		// OK   - 01.07.2020_R100000000_0059
+		setUpHappyStubs();
+		setUpBadStubs();
+		copyFileFromClasspathToInngaaende(ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION + ".enc.zip");
 
-        await().atMost(25, SECONDS).untilAsserted(() -> {
-            try {
-                assertThat(Files.list(sshdPath.resolve(FEILMAPPE)
-                        .resolve(ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION))
-                        .collect(Collectors.toList())).hasSize(4);
-            } catch (NoSuchFileException e) {
-                fail();
-            }
-        });
+		await().atMost(25, SECONDS).untilAsserted(() -> {
+			try {
+				assertThat(Files.list(sshdPath.resolve(FEILMAPPE)
+						.resolve(ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION))
+						.collect(Collectors.toList())).hasSize(4);
+			} catch (NoSuchFileException e) {
+				fail();
+			}
+		});
 
-        final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE).resolve(ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION))
-                .map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
-                .collect(Collectors.toList());
-        assertTrue(feilmappeContents.containsAll(List.of(
-                "01.07.2020_R100000000_0003.enc.zip",
-                "01.07.2020_R100000000_0004.enc.zip",
-                "01.07.2020_R100000000_0005.enc.zip",
-                "01.07.2020_R100000000_0006.enc.zip"
-        )));
-        verify(exactly(56), putRequestedFor(urlMatching(URL_DOKARKIV_JOURNALPOST_GEN)));
-    }
+		final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE).resolve(ZIP_FILE_NAME_ORDERED_XML_FIRST_NO_EXTENSION))
+				.map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
+				.collect(Collectors.toList());
+		assertTrue(feilmappeContents.containsAll(List.of(
+				"01.07.2020_R100000000_0003.enc.zip",
+				"01.07.2020_R100000000_0004.enc.zip",
+				"01.07.2020_R100000000_0005.enc.zip",
+				"01.07.2020_R100000000_0006.enc.zip"
+		)));
+		verify(exactly(56), putRequestedFor(urlMatching(URL_DOKARKIV_JOURNALPOST_GEN)));
+	}
 
-    @Test
-    public void shouldMoveZipToFeilomraadeWhenBadPassword() throws IOException {
+	@Test
+	public void shouldMoveZipToFeilomraadeWhenBadPassword() throws IOException {
 
-        //ZipException: Bad password
-        //should be sent to feilmappe
+		//ZipException: Bad password
+		//should be sent to feilmappe
 
-        copyFileFromClasspathToInngaaende(ZIP_FILENAME_NO_EXTENSION_BAD_PASSWORD + ".enc.zip");
+		copyFileFromClasspathToInngaaende(ZIP_FILENAME_NO_EXTENSION_BAD_PASSWORD + ".enc.zip");
 
-        await().atMost(15, SECONDS).untilAsserted(() -> {
-            try {
-                final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE))
-                        .map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
-                        .collect(Collectors.toList());
-                assertTrue(feilmappeContents.contains(ZIP_FILENAME_NO_EXTENSION_BAD_PASSWORD + ".enc.zip"));
-            } catch (NoSuchFileException e) {
-                fail();
-            }
-        });
-    }
+		await().atMost(15, SECONDS).untilAsserted(() -> {
+			try {
+				final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE))
+						.map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
+						.collect(Collectors.toList());
+				assertTrue(feilmappeContents.contains(ZIP_FILENAME_NO_EXTENSION_BAD_PASSWORD + ".enc.zip"));
+			} catch (NoSuchFileException e) {
+				fail();
+			}
+		});
+	}
 
-    @Test
-    public void shouldMoveZipToFeilomraadeWhenNotEncryptedEncFile() throws IOException {
+	@Test
+	public void shouldMoveZipToFeilomraadeWhenNotEncryptedEncFile() throws IOException {
 
-        //ZipException: En .enc-file kom inn men filene er ukrypterte
-        //should be sent to feilmappe
+		//ZipException: En .enc-file kom inn men filene er ukrypterte
+		//should be sent to feilmappe
 
-        copyFileFromClasspathToInngaaende(ZIP_FILE_NAME_NOT_ENCRYPTED_ENC + ".enc.zip");
+		copyFileFromClasspathToInngaaende(ZIP_FILE_NAME_NOT_ENCRYPTED_ENC + ".enc.zip");
 
-        await().atMost(15, SECONDS).untilAsserted(() -> {
-            try {
-                final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE))
-                        .map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
-                        .collect(Collectors.toList());
-                assertTrue(feilmappeContents.contains(ZIP_FILE_NAME_NOT_ENCRYPTED_ENC + ".enc.zip"));
-            } catch (NoSuchFileException e) {
-                fail();
-            }
-        });
-    }
+		await().atMost(15, SECONDS).untilAsserted(() -> {
+			try {
+				final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE))
+						.map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
+						.collect(Collectors.toList());
+				assertTrue(feilmappeContents.contains(ZIP_FILE_NAME_NOT_ENCRYPTED_ENC + ".enc.zip"));
+			} catch (NoSuchFileException e) {
+				fail();
+			}
+		});
+	}
 
-    @Test
-    public void shouldMoveZipToFeilomraadeWhenBadEncryption() throws IOException {
+	@Test
+	public void shouldMoveZipToFeilomraadeWhenBadEncryption() throws IOException {
 
-        //ZipException: Filene er ikke kryptert med AES men en annen krypteringsmetode
-        //should be sent to feilmappe
+		//ZipException: Filene er ikke kryptert med AES men en annen krypteringsmetode
+		//should be sent to feilmappe
 
-        copyFileFromClasspathToInngaaende(ZIP_FILENAME_NO_EXTENSION_BAD_ENCRYPTION + ".enc.zip");
+		copyFileFromClasspathToInngaaende(ZIP_FILENAME_NO_EXTENSION_BAD_ENCRYPTION + ".enc.zip");
 
-        await().atMost(15, SECONDS).untilAsserted(() -> {
-            try {
-                final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE))
-                        .map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
-                        .collect(Collectors.toList());
-                assertTrue(feilmappeContents.contains(ZIP_FILENAME_NO_EXTENSION_BAD_ENCRYPTION + ".enc.zip"));
-            } catch (NoSuchFileException e) {
-                fail();
-            }
-        });
-    }
+		await().atMost(15, SECONDS).untilAsserted(() -> {
+			try {
+				final List<String> feilmappeContents = Files.list(sshdPath.resolve(FEILMAPPE))
+						.map(p -> FilenameUtils.getName(p.toAbsolutePath().toString()))
+						.collect(Collectors.toList());
+				assertTrue(feilmappeContents.contains(ZIP_FILENAME_NO_EXTENSION_BAD_ENCRYPTION + ".enc.zip"));
+			} catch (NoSuchFileException e) {
+				fail();
+			}
+		});
+	}
 
-    private void setUpHappyStubs() {
-        stubFor(put(urlMatching(URL_DOKARKIV_JOURNALPOST_GEN))
-                .willReturn(aResponse().withStatus(HttpStatus.OK.value())
-                        .withHeader(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                        .withBody("{}")));
-    }
+	private void preparePath(Path path) throws IOException {
+		if (!Files.exists(path)) {
+			Files.createDirectory(path);
+		} else {
+			FileUtils.cleanDirectory(path.toFile());
+		}
+	}
 
-    private void setUpBadStubs() {
-        stubFor(put(urlMatching(URL_DOKARKIV_JOURNALPOST_BAD_REQUEST))
-                .willReturn(aResponse().withStatus(HttpStatus.BAD_REQUEST.value())));
-    }
-
-    private void preparePath(Path path) throws IOException {
-        if (!Files.exists(path)) {
-            Files.createDirectory(path);
-        } else {
-            FileUtils.cleanDirectory(path.toFile());
-        }
-    }
-
-    private void copyFileFromClasspathToInngaaende(final String zipfilename) throws IOException {
-        Files.copy(new ClassPathResource(zipfilename).getInputStream(), sshdPath.resolve(INNGAAENDE).resolve(zipfilename));
-    }
+	private void copyFileFromClasspathToInngaaende(final String zipfilename) throws IOException {
+		Files.copy(new ClassPathResource(zipfilename).getInputStream(), sshdPath.resolve(INNGAAENDE).resolve(zipfilename));
+	}
 }
