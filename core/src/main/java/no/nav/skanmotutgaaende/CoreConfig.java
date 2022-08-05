b57aa7a21bd4e04c7efa4af33f44283d3dac17f9
Joakim Borgersen <joakim.borgersen@nav.no>
2022-08-05 12:38:03 +0200

    add HttpClientConnectionManager til coreconfig

--- core/src/main/java/no/nav/skanmotutgaaende/CoreConfig.java
+++ core/src/main/java/no/nav/skanmotutgaaende/CoreConfig.java
@@ -1,9 +1,35 @@
 package no.nav.skanmotutgaaende;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @ComponentScan
 @Configuration
 public class CoreConfig {
+	@Bean
+	ClientHttpRequestFactory requestFactory(HttpClient httpClient) {
+		return new HttpComponentsClientHttpRequestFactory(httpClient);
+	}
+
+	@Bean
+	HttpClient httpClient(HttpClientConnectionManager connectionManager) {
+		return HttpClients.custom()
+				.setConnectionManager(connectionManager)
+				.build();
+	}
+
+	@Bean
+	HttpClientConnectionManager httpClientConnectionManager() {
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+		connectionManager.setMaxTotal(400);
+		connectionManager.setDefaultMaxPerRoute(100);
+		return connectionManager;
+	}
 }
