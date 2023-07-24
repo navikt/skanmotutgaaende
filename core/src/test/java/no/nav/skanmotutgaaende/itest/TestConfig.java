e6fd7ac53a03d226f607330bfa7bda4a39d2f552
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-07-24 13:47:39 +0200

    MMA-6975 Oppdatert avhengigheter, optimalisert
    * Oppdatert camel, sshd-mina, etc
    * JAXBContext implementasjoner er trådsikre, instansieres kun en gang
    * Fikset sshd oppsett

--- core/src/test/java/no/nav/skanmotutgaaende/itest/TestConfig.java
+++ core/src/test/java/no/nav/skanmotutgaaende/itest/TestConfig.java
@@ -10,12 +10,12 @@ import no.nav.skanmotutgaaende.metrics.DokCounter;
 import org.apache.camel.CamelContext;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
 import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
+import org.apache.sshd.common.keyprovider.ClassLoadableResourceKeyPairProvider;
+import org.apache.sshd.scp.server.ScpCommandFactory;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.auth.UserAuthNoneFactory;
-import org.apache.sshd.server.config.keys.AuthorizedKeysAuthenticator;
-import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
-import org.apache.sshd.server.scp.ScpCommandFactory;
-import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+import org.apache.sshd.server.auth.pubkey.AcceptAllPublickeyAuthenticator;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
 import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -27,12 +27,9 @@ import org.springframework.context.annotation.Primary;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static java.lang.Integer.parseInt;
 import static java.util.Collections.singletonList;
 import static org.mockito.ArgumentMatchers.anyString;
 
@@ -40,75 +37,75 @@ import static org.mockito.ArgumentMatchers.anyString;
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigurationProperties({
-        SkanmotutgaaendeProperties.class,
-        IMVaultProperties.class
+		SkanmotutgaaendeProperties.class,
+		IMVaultProperties.class
 })
 @Import({TestConfig.CamelTestStartupConfig.class, TestConfig.SshdSftpServerConfig.class, CoreConfig.class, DokCounter.class})
 public class TestConfig {
 
-    @Bean
-    @Primary
-    AzureTokenConsumer azureTokenConsumer() {
-        AzureTokenConsumer azureTokenConsumer = Mockito.mock(AzureTokenConsumer.class);
-        Mockito.when(azureTokenConsumer.getClientCredentialToken(anyString())).thenReturn(
-                TokenResponse.builder()
-                        .access_token("dummy")
-                        .build()
-        );
+	@Bean
+	@Primary
+	AzureTokenConsumer azureTokenConsumer() {
+		AzureTokenConsumer azureTokenConsumer = Mockito.mock(AzureTokenConsumer.class);
+		Mockito.when(azureTokenConsumer.getClientCredentialToken(anyString())).thenReturn(
+				TokenResponse.builder()
+						.access_token("dummy")
+						.build()
+		);
 
-        return azureTokenConsumer;
-    }
+		return azureTokenConsumer;
+	}
 
-    @Configuration
-    static class CamelTestStartupConfig {
-        private final AtomicInteger sshServerStartupCounter = new AtomicInteger(0);
-        // Hindre at sshserver ikke er klar før camel starter å behandle.
-        @Bean
-        CamelContextConfiguration contextConfiguration(SshServer sshServer) {
-            return new CamelContextConfiguration() {
+	@Configuration
+	static class CamelTestStartupConfig {
+		private final AtomicInteger sshServerStartupCounter = new AtomicInteger(0);
 
-                @Override
-                public void beforeApplicationStart(CamelContext camelContext) {
-                    while(!sshServer.isStarted() && sshServerStartupCounter.get() <= 5) {
-                        try {
-                            // Busy wait
-                            Thread.sleep(1000);
-                            log.info("Forsøkt å starte sshserver. retry=" + sshServerStartupCounter.getAndIncrement());
-                        } catch (InterruptedException e) {
-                            // noop
-                        }
-                    }
-                }
+		// Hindre at sshserver ikke er klar før camel starter å behandle.
+		@Bean
+		CamelContextConfiguration contextConfiguration(SshServer sshServer) {
+			return new CamelContextConfiguration() {
 
-                @Override
-                public void afterApplicationStart(CamelContext camelContext) {
+				@Override
+				public void beforeApplicationStart(CamelContext camelContext) {
+					while (!sshServer.isStarted() && sshServerStartupCounter.get() <= 5) {
+						try {
+							// Busy wait
+							Thread.sleep(1000);
+							log.info("Forsøkt å starte sshserver. retry=" + sshServerStartupCounter.getAndIncrement());
+						} catch (InterruptedException e) {
+							// noop
+						}
+					}
+				}
 
-                }
-            };
-        }
-    }
+				@Override
+				public void afterApplicationStart(CamelContext camelContext) {
 
-    @Configuration
-    static class SshdSftpServerConfig {
-        @Bean
-        public Path sshdPath() throws IOException {
-            return Files.createTempDirectory("sshd");
-        }
+				}
+			};
+		}
+	}
 
-        @Bean(initMethod = "start", destroyMethod = "stop")
-        public SshServer sshServer(final Path sshdPath) {
-            String sftpPort = String.valueOf(ThreadLocalRandom.current().nextInt(2000, 2999));
-            System.setProperty("skanmotutgaaende.sftp.port", sftpPort);
+	@Configuration
+	static class SshdSftpServerConfig {
+		@Bean
+		public Path sshdPath() throws IOException {
+			return Files.createTempDirectory("sshd");
+		}
 
-            SshServer sshd = SshServer.setUpDefaultServer();
-            sshd.setPort(Integer.parseInt(sftpPort));
-            sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(Path.of("src/test/resources/sftp/itest.ser")));
-            sshd.setCommandFactory(new ScpCommandFactory());
-            sshd.setSubsystemFactories(singletonList(new SftpSubsystemFactory()));
-            sshd.setPublickeyAuthenticator(new AuthorizedKeysAuthenticator(Paths.get("src/test/resources/sftp/itest_valid.pub")));
-            sshd.setUserAuthFactories(singletonList(new UserAuthNoneFactory()));
-            sshd.setFileSystemFactory(new VirtualFileSystemFactory(sshdPath));
-            return sshd;
-        }
-    }
+		@Bean(initMethod = "start", destroyMethod = "stop")
+		public SshServer sshServer(Path sshdPath,
+								   SkanmotutgaaendeProperties skanmotutgaaendeProperties) {
+			SshServer sshd = SshServer.setUpDefaultServer();
+			sshd.setPort(parseInt(skanmotutgaaendeProperties.getSftp().getPort()));
+			sshd.setKeyPairProvider(new ClassLoadableResourceKeyPairProvider("sftp/server_id_rsa"));
+			sshd.setCommandFactory(new ScpCommandFactory());
+			sshd.setSubsystemFactories(singletonList(new SftpSubsystemFactory()));
+			// aksepterer alle public keys som presenteres, behøver ikke authorized_keys
+			sshd.setPublickeyAuthenticator(AcceptAllPublickeyAuthenticator.INSTANCE);
+			sshd.setUserAuthFactories(singletonList(new UserAuthNoneFactory()));
+			sshd.setFileSystemFactory(new VirtualFileSystemFactory(sshdPath));
+			return sshd;
+		}
+	}
 }
