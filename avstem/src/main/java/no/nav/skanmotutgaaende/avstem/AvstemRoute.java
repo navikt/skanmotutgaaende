6a0e7b7137108acd04ebc2223703134fa5e22d27
Joakim Borgersen <joakim.borgersen@nav.no>
2025-06-19 14:25:22 +0200

    Prøv med camelcontext

--- avstem/src/main/java/no/nav/skanmotutgaaende/avstem/AvstemRoute.java
+++ avstem/src/main/java/no/nav/skanmotutgaaende/avstem/AvstemRoute.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.skanmotutgaaende.MdcSetterProcessor;
 import no.nav.skanmotutgaaende.RemoveMdcProcessor;
 import no.nav.skanmotutgaaende.jira.OpprettJiraService;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.PredicateBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.file.GenericFileOperationFailedException;
@@ -18,7 +19,6 @@ import static no.nav.skanmotutgaaende.mdc.MDCConstants.EXCHANGE_AVSTEMT_DATO;
 import static org.apache.camel.Exchange.FILE_NAME;
 import static org.apache.camel.LoggingLevel.ERROR;
 import static org.apache.camel.LoggingLevel.INFO;
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
 @Slf4j
 @Component
@@ -30,13 +30,16 @@ public class AvstemRoute extends RouteBuilder {
 	public static String FEIL_ROUTE = "direct:feil";
 	public static String FERDIG_ROUTE = "direct:ferdig";
 
+	private final CamelContext camelContext;
+
 	@Value("${skanmotutgaaende.avstem.delay}")
 	private static int delayMs;
 
 	private final OpprettJiraService opprettJiraService;
 
-	public AvstemRoute(AvstemController avstemController, OpprettJiraService opprettJiraService) {
+	public AvstemRoute(AvstemController avstemController, CamelContext camelContext, OpprettJiraService opprettJiraService) {
 		this.avstemController = avstemController;
+		this.camelContext = camelContext;
 		this.opprettJiraService = opprettJiraService;
 	}
 
@@ -61,41 +64,58 @@ public class AvstemRoute extends RouteBuilder {
 						"&noop=true", CONNECTION_TIMEOUT)
 				.routeId("verifiser_avstem_routeid")
 				.autoStartup("{{skanmotutgaaende.avstem.startup}}")
-				.log(INFO, log, "Skanmotutgaaende starter cron jobb for å avstemme referanser...")
+				.log(INFO, log, "+++++++Skanmotutgaaende starter cron jobb for å avstemme referanser...")
 				.process(new MdcSetterProcessor())
 				.process(exchange -> {
 					String fileName = exchange.getIn().getHeader(FILE_NAME, String.class);
 					log.info("Mottok exchange med filnavn: " + fileName);
 					log.info("Headers: " + exchange.getIn().getHeaders().toString());
-					log.info("content: " + exchange.getIn().getBody(String.class));
 				})
 				.choice()
 					.when(PredicateBuilder.or(header(FILE_NAME).isNull(),(body().isNull() )))
-					.process(exchange -> exchange.setProperty(EXCHANGE_AVSTEMT_DATO, finnForrigeVirkedag()))
-					.log(ERROR, log, "Skanmotutgaaende fant ikke avstemmingsfil for ${exchangeProperty." + EXCHANGE_AVSTEMT_DATO + "}. Undersøk tilfellet og se opprettet Jira-sak.")
-					.bean(opprettJiraService)
-					.log(INFO, log, "Skanmotutgaaende opprettet jira-sak med key=${body.jiraIssueKey} for manglende avstemmingsfil.")
+						.process(exchange -> exchange.setProperty(EXCHANGE_AVSTEMT_DATO, finnForrigeVirkedag()))
+						.log(ERROR, log, "Skanmotutgaaende fant ikke avstemmingsfil for ${exchangeProperty." + EXCHANGE_AVSTEMT_DATO + "}. Undersøk tilfellet og se opprettet Jira-sak.")
+						.bean(opprettJiraService)
+						.log(INFO, log, "Skanmotutgaaende opprettet jira-sak med key=${body.jiraIssueKey} for manglende avstemmingsfil.")
+					.otherwise()
+						.log(INFO, log, "Skanmotutgaaende fant avstemmingsfil " + header(FILE_NAME))
 				.endChoice()
+				.process(exchange -> {
+					try {
+						camelContext.getRouteController().startRoute("avstem_routeid");
+						log.info("Route 'avstem_routeid' has been started after processing.");
+					} catch (Exception e) {
+						throw new RuntimeException("Failed to start second route", e);
+					}
+				})
 				.process(new RemoveMdcProcessor())
+				.delay(3000)
 				.end();
 
 
 				from("{{skanmotutgaaende.endpointuri}}/{{skanmotutgaaende.filomraade.avstemmappe}}" +
 								"?{{skanmotutgaaende.endpointconfig}}" +
 								"&antInclude=*.txt,*.TXT"+
-								"&maxMessagesPerPoll=10" +
-						"&scheduler=spring&scheduler.cron={{skanmotutgaaende.utgaaende.schedule}}")
-				.autoStartup("{{skanmotutgaaende.avstem.startup}}")
+								"&maxMessagesPerPoll=10")
+				.autoStartup(false)
 				.routeId("avstem_routeid")
-				.delay(delayMs)
 				.process(new MdcSetterProcessor())
-				.log(INFO, log, "Skanmotutgaaende starter cron jobb for å avstemme referanser")
+				.log(INFO, log, "-------Skanmotutgaaende starter jobb for å avstemme referanser")
 				.aggregate(constant(true), new GroupedExchangeAggregationStrategy())
-				 .completionTimeout(500)
+				.completionTimeout(500)
 				.convertBodyTo(Set.class)
 				.bean(avstemController)
 				.log(INFO, log, "Skanmotutgaaende har kjørt cron jobb for å avstemme referanser")
 				.process(new RemoveMdcProcessor())
+				.process(exchange -> {
+					// Stop the route after processing
+					try {
+						camelContext.getRouteController().stopRoute("avstem_routeid");
+						log.info("Route 'avstem_routeid' has been stopped after processing.");
+					} catch (Exception e) {
+						throw new RuntimeException("Failed to stop route", e);
+					}
+				})
 				.end();
 
 		from(FERDIG_ROUTE)
